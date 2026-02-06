b54a62e9f5c2865f2052c74e17fa3f709dde49d1
Ashutosh Ayush <jbnashu297@gmail.com>
2026-02-06 22:32:15 +0530

    declarations file added

---
+++ midnight-pookie-–-an-ethereal-dark-valentine/declarations.d.ts
@@ -0,0 +1,23 @@
+
+declare module 'react' {
+    import * as React from 'react';
+    export default React;
+    export * from 'react';
+}
+
+declare module 'react-dom/client' {
+    export * from 'react-dom/client';
+}
+
+declare module 'framer-motion' {
+    export * from 'framer-motion';
+}
+
+declare module 'lucide-react' {
+    export * from 'lucide-react';
+}
+
+declare module 'https://cdn.skypack.dev/canvas-confetti' {
+    const confetti: any;
+    export default confetti;
+}
